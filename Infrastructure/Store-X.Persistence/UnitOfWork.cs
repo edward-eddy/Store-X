271ef49c224a740d539883c599d3843bc2996240
edwar <edwardsameh99@gmail.com>
2025-10-20 13:10:54 +0300

    07 Unit Of Work

---
+++ Infrastructure/Store-X.Persistence/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Store_X.Domain.Contracts;
+using Store_X.Domain.Entities;
+using Store_X.Persistence.Data.Contexts;
+using Store_X.Persistence.Repositories;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_X.Persistence
+{
+    public class UnitOfWork(StoreDbContext _context) : IUnitOfWork
+    {
+        //private Dictionary<string, object> _repositories = new Dictionary<string, object>();
+
+        //public IGenericRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity : BaseEntity<TKey>
+        //{
+        //    var dKey = typeof(TEntity).Name;
+        //    if (!_repositories.ContainsKey(dKey))
+        //    {
+        //        var repository = new GenericRepository<TKey, TEntity>(_context);
+        //        _repositories.Add(dKey, repository);
+        //    }
+        //    return (IGenericRepository<TKey, TEntity>)_repositories[dKey];
+        //}
+
+        private ConcurrentDictionary<string, object> _repositories = new ConcurrentDictionary<string, object>();
+        public IGenericRepository<TKey, TEntity> GetRepository<TKey, TEntity>() where TEntity : BaseEntity<TKey>
+        {
+            return (IGenericRepository<TKey, TEntity>)_repositories.GetOrAdd(typeof(TEntity).Name, new GenericRepository<TKey, TEntity>(_context));
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
