780618db349f91c061fc78d40e8acf4a80d4dddc
edwar <edwardsameh99@gmail.com>
2025-10-20 12:46:42 +0300

    06 Generic Repository

---
+++ Infrastructure/Store-X.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Store_X.Domain.Contracts;
+using Store_X.Domain.Entities;
+using Store_X.Persistence.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_X.Persistence.Repositories
+{
+    public class GenericRepository<TKey, TEntity>(StoreDbContext _context) : IGenericRepository<TKey, TEntity> where TEntity : BaseEntity<TKey>
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool removeTracker = false)
+        {
+            return removeTracker
+                ? await _context.Set<TEntity>().ToListAsync()
+                : await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(TKey key)
+        {
+            return await _context.Set<TEntity>().FindAsync(key);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
