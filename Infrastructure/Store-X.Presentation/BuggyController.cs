4d0426a973514047cc402009681a9a0b230b3dd4
edwar <edwardsameh99@gmail.com>
2025-10-29 04:10:02 +0300

    01 Error Types at API

---
+++ Infrastructure/Store-X.Presentation/BuggyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Store_X.Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BuggyController : ControllerBase
+    {
+        [HttpGet("404")]
+        public IActionResult GetNotFoundRequest()
+        {
+            var id = 2;
+            return NotFound(new { message = $"Couldn't Find Product With Id: {id}", statusCode = 400, portal = 5000 });
+        }
+
+        [HttpGet("500")]
+        public IActionResult GetServerErrorRequest()
+        {
+            throw new Exception("Server Error 500");
+        }
+
+        [HttpGet("400")]
+        public IActionResult GetBadRequest()
+        {
+            var id = 2;
+            return BadRequest($"Invalid Login");
+        }
+
+        [HttpGet("400/{id}")]
+        public IActionResult GetBadRequest(int id)
+        {
+            id = 2;
+            return BadRequest(new { message = $"Invalid Login for user {id}", statusCode = 400 });
+        }
+
+        [HttpGet("401")]
+        public IActionResult GetUnauthorizedRequest()
+        {
+            return Unauthorized();
+        }
+    }
+}
--- Infrastructure/Store-X.Presentation/ProductsController.cs
+++ Infrastructure/Store-X.Presentation/ProductsController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Store_X.Services_Abstractions;
 using Store_X.Shared.Dtos.Products;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Store_X.Presentation
 {
