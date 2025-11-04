c05c3d8684971ad5a51e4619a709989b1323d70a
edwar <edwardsameh99@gmail.com>
2025-11-04 14:30:15 +0200

    07 Cache Repository

---
+++ Core/Store-X.Domain/Contracts/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Store_X.Domain.Contracts
+{
+    public interface ICacheRepository
+    {
+        Task<string> GetAsync(string key);
+        Task<string> SetAsync(string key, object value, TimeSpan lifeTime);
+    }
+}
