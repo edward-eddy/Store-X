a4aec2e2f0aaf9c78a39046241dd4a3f295a6109
edwar <edwardsameh99@gmail.com>
2025-11-11 20:58:32 +0200

    03 Order Modules - Configurations

---
+++ Infrastructure/Store-X.Persistence/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store_X.Domain.Entities.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_X.Persistence.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(O => O.ShippingAddress);
+
+            builder.HasOne(O => O.DeliveryMethod)
+                    .WithMany()
+                    .HasForeignKey(O => O.DeliveryMethodId);
+
+            builder.HasMany(O => O.Items)
+                   .WithOne()
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(O => O.SubTotal).HasColumnType("decimal(18,2)");
+        }
+    }
+}
--- Infrastructure/Store-X.Persistence/Data/Contexts/StoreDbContext.cs
+++ Infrastructure/Store-X.Persistence/Data/Contexts/StoreDbContext.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Store_X.Domain.Entities.Orders;
 using Store_X.Domain.Entities.Products;
 using System.Reflection;
 
 namespace Store_X.Persistence.Data.Contexts
 {
-    public class StoreDbContext : DbContext
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
     {
-        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
-        {
-
-        }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
+
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
