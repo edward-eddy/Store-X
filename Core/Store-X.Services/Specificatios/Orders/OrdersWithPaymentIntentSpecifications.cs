41fee5bc1d7fb1a54cf9d01cdc5965ca594f0f85
edwar <edwardsameh99@gmail.com>
2025-11-26 12:10:34 +0200

    08 Refactoring Create Order

---
+++ Core/Store-X.Services/Specificatios/Orders/OrdersWithPaymentIntentSpecifications.cs
@@ -0,0 +1,16 @@
+﻿using Store_X.Domain.Entities.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_X.Services.Specificatios.Orders
+{
+    public class OrdersWithPaymentIntentSpecifications : BaseSpecifications<Guid, Order>
+    {
+        public OrdersWithPaymentIntentSpecifications(string paymentIntentId) : base(O => O.PaymentIntentId == paymentIntentId)
+        {
+        }
+    }
+}
